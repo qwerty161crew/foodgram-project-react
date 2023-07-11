c39f0d79744d42715bdf82f5af55438a000f3c91
Алексей Матвеев <qwerty161crew@gmail.com>
2023-07-11 16:19:37 +0300

    доработан Docker

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -4,10 +4,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'), name='api'),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc',
     ),
+    path('', include('api.urls'), name='api'),
 ]
